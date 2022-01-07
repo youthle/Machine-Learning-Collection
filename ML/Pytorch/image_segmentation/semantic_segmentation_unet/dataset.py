6ae0418a5755ba4838d9a61d1ce7fae9e6a69200
youthle <49577873+youthle@users.noreply.github.com>
2022-01-07 11:57:24 -0600

    Update dataset.py

--- ML/Pytorch/image_segmentation/semantic_segmentation_unet/dataset.py
+++ ML/Pytorch/image_segmentation/semantic_segmentation_unet/dataset.py
@@ -3,12 +3,12 @@ from PIL import Image
 from torch.utils.data import Dataset
 import numpy as np
 
-class CarvanaDataset(Dataset):
+class CarvanaDataset(Dataset):  #继承自Dataset
     def __init__(self, image_dir, mask_dir, transform=None):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.transform = transform
-        self.images = os.listdir(image_dir)
+        self.images = os.listdir(image_dir)  # list all files in the image_dir folder
 
     def __len__(self):
         return len(self.images)
@@ -16,8 +16,8 @@ class CarvanaDataset(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.image_dir, self.images[index])
         mask_path = os.path.join(self.mask_dir, self.images[index].replace(".jpg", "_mask.gif"))
-        image = np.array(Image.open(img_path).convert("RGB"))
-        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+        image = np.array(Image.open(img_path).convert("RGB"))  #不一定需要convert to RGB, 这步转npArray是因为要用PIL
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32) #L是因为mask是grayscale image
         mask[mask == 255.0] = 1.0
 
         if self.transform is not None:
